0ef6e11c0aef21f619742956242c1be280c1ae79
yang <shiokoy1209@gmail.com>
2021-05-10 18:03:47 +0800

    注册$router

--- src/yvue-router/yvue-router.js
+++ src/yvue-router/yvue-router.js
@@ -3,17 +3,37 @@ let Vue
 //1.实现插件
 class VueRouter {
   constructor(options) {
-    this.$options = options;
+    this.options = options;
   }
 }
 
 VueRouter.install = function (_Vue) {
   Vue = _Vue // 相当于一个引用，将来打包时不会将vue打包进来
   
+  // 注册router实例
+  // 通过全局混入，访问Vue实例
+  Vue.mixin({
+    beforeCreate() {
+      // 仅在根组件创建时执行一次
+      if(this.$options.router) {
+        Vue.prototype.$router = this.$options.router
+      }
+    }
+  })
+  
   // 全局注册router-view  router-link
   Vue.component('router-view', {
+    // url => component
+    // url
+    // window.location.hash
+    // router: this.$router
     render(h) {
-      return h('div', 'router内容')
+      let component = null
+      console.log(this.$router)
+      const {options} = this.$router
+      console.log(options);
+      component = options.routes[0].component
+      return h(component)
     }
   })
   Vue.component('router-link', {
