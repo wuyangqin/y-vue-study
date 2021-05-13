7197c5dc4b0a61173cf31d1128eb61b87ddf2366
yang <shiokoy1209@gmail.com>
2021-05-13 17:46:40 +0800

    解析插值表达式

--- yvue/yvue.js
+++ yvue/yvue.js
@@ -5,12 +5,10 @@ function defineReactive(obj, key, value) {
   
   Object.defineProperty(obj, key, {
     get() {
-      console.log('get', key, value)
       return value
     },
     set(newValue) {
       if (value !== newValue) {
-        console.log('set', key, value)
         value = newValue
         // 新值如果是对象，仍然需要递归遍历处理
         observe(newValue);
@@ -41,9 +39,28 @@ function proxy(vm) {
   })
 }
 
+
+class YVue {
+  constructor(options) {
+    // 0.保存选项
+    this.$options = options
+    this.$data = options.data
+    
+    // 1.对data做响应式处理
+    observe(options.data);
+    
+    // 2. 代理，将data中的key代理到Vue实例中
+    proxy(this)
+    
+    // 3.编译
+    new Compile(options.el, this)
+  }
+}
+
+// 执行数据响应化
 class Observer {
   constructor(obj) {
-    // 判断obj的类型
+    // 判断obj的类型，做响应处理
     if (Array.isArray(obj)) {
       // todo
     } else {
@@ -57,16 +74,51 @@ class Observer {
   }
 }
 
-class YVue {
-  constructor(options) {
-    // 0.保存选项
-    this.$options = options
-    this.$data = options.data
-    
-    // 1.对data做响应式处理
-    observe(options.data);
-    
-    // 2. 代理，将data中的key代理到Vue实例中
-    proxy(this)
+class Compile {
+  constructor(el, vm) {
+    this.$vm = vm
+    this.$el = document.querySelector(el)
+    if (this.$el) {
+      this.compile(this.$el);
+    }
   }
+  
+  // 遍历node，判断节点类型，做不同处理
+  compile(node) {
+    const childrenNodes = node.childNodes
+    Array.from(childrenNodes).forEach(n => {
+      // 如果是元素则编译元素，如果是文本，则判断是否有{{}}，编译插值文本
+      if (this.isElement(n)) {
+        // 编译元素
+        this.compileElement(n);
+        if (n.childNodes.length > 0) {
+          this.compile(n);
+        }
+      } else if (this.isInter(n)) {
+        // 编译插值表达式
+        this.compileInter(n)
+      }
+    })
+  }
+  
+  
+  // 是否为元素
+  isElement(n) {
+    return n.nodeType === 1
+  }
+  
+  // 判断是否为插值表达式（形如{{}}）
+  isInter(n) {
+    return n.nodeType === 3 && /\{\{(.*)\}\}/.test(n.textContent)
+  }
+  
+  // 编译插值文本 {{ooxx}}
+  compileInter(n) {
+    n.textContent = this.$vm[RegExp.$1]
+  }
+  
+  //编译元素：遍历它的所有特性，看是否k-开头指令，或者@事件
+  compileElement(n) {
+  }
+  
 }
